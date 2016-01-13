29569a33fa850a24d285e85bcd9f00efa1f7598e
jackson cafazzo and Pablo Vera <doctorjuno@gmail.com>
2016-01-13 11:58:19 -0800

    simplify the js code

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,21 @@ $(document).ready(function() {
   $("form#celebrity").submit(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
+    $("#kristen").hide();
+    $("#jessica").hide();
+    $('#david').hide();
 
     if (gender === 'male') {
-      $('#celeb').empty();
-      $('#celeb').append('David');
-      $("#kristen").hide();
-      $("#jessica").hide();
+      $('#celeb').text('David');
       $('#david').show();
     }
     if (gender === "female") {
       if (age <= 20) {
-        $('#celeb').empty();
-        $('#celeb').append('Kristen');
-        $("#jessica").hide();
-        $('#david').hide();
+        $('#celeb').text('Kristen');
         $("#kristen").show();
       }
       if (age >20) {
-        $('#celeb').empty();
-        $('#celeb').append("Jessica");
-        $('#david').hide();
-        $("#kristen").hide();
+        $('#celeb').text("Jessica");
         $("#jessica").show();
       }
     }
